bcb22514aa6a4c49429114dcfb2ff8e32cf4d96e
Sue Smith <sue@benormal.info>
2025-06-27 16:03:09 +0000

    edit

--- origin/vite.config.js
+++ origin/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig(async ({ command, mode }) => {
   return {
     build: {
-      outDir: "./docs",
+      outDir: "../docs",
       rollupOptions: {
         input: {
           main: 'index.html'
